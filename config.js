98babdc81ff041d63c72484ec26308a9e5b54fc8
Daniel P Jerrish <minecliffsdanie.l@gmail.com>
2025-08-01 15:18:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 module.exports = {
     prefix: '!',
-    clientId: '1372777810812473426', // Your bot's client ID
+    clientId: 'YOUR_BOT_CLIENT_ID',
     nodes: [{
-        host: "41.216.188.215",
-        password: "nexorafox",
-        port: 5685,
-        secure: false,
+        host: "YOUR_LAVALINK_HOST",
+        password: "YOUR_LAVALINK_PASSWORD",
+        port: 443,
+        secure: true,
         name: "Main Node"
     }],
     spotify: {
-        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
-        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+        clientId: "YOUR_SPOTIFY_CLIENT_ID",
+        clientSecret: "YOUR_SPOTIFY_CLIENT_SECRET"
     },
-    botToken: "MTM3Mjc3NzgxMDgxMjQ3MzQyNg.GQzW24.uPUTgyzz0_tadXdfJpWdAciLVU0AJRsytwpkX0",
+    botToken: "YOUR_DISCORD_BOT_TOKEN",
     embedColor: "#0061ff"
 };
-
